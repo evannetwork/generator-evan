6943d0ea7250db5faef48fac0f6bc037168c0f23
wulfraem <wulfraem@users.noreply.github.com>
2019-01-11 13:16:21 +0100

    link all config files into actionhero; code cleanup

--- generators/agent/templates/root/gulp/smart-agents.js
+++ generators/agent/templates/root/gulp/smart-agents.js
@@ -17,13 +17,16 @@
 const gulp = require('gulp')
 const path = require('path')
 const fs = require('fs')
-const spawn = require('child_process').spawn
 
 gulp.task('link-agents', () => {
   const plugins = path.resolve(process.cwd(),'node_modules/@evan.network/edge-server-seed/config/plugins.js')
   const source_plugins = '../../../../scripts/config/plugins.js'
-  try { fs.unlinkSync(plugins) } catch(e){}
-  fs.symlink(source_plugins, plugins,  () => { console.log('Replaced ', plugins) })
+  try {
+    fs.unlinkSync(plugins)
+  } catch(e) {
+    // silent throw
+  }
+  fs.symlink(source_plugins, plugins, () => { console.log('Replaced ', plugins) })
   gulp.src('smart-agent-*/config/*.js', { read: false })
     .on('data', d => {
       const c = d.history[0]
@@ -33,21 +36,17 @@ gulp.task('link-agents', () => {
       const dd = p.dir.slice(0,-7)
       const dlink = 'node_modules/@evan.network/edge-server-seed/node_modules/' + agent_name
 
-      if(agent_name === p.name) console.log("Linked plugin: ",link,' ', dd, ' ', dlink )
-
-      if(agent_name === p.name) {
-        fs.symlink(c, link, (err) => {
-          if(err) {
-            console.log("ERROR linking plugin: ", err )
-          } else {
-            console.log("Linked plugin: ", agent_name )
-          }
-        })
-        fs.symlink(dd, dlink, (err) => {
-          if(err) {
-            console.log("ERROR linking plugin: ", err )
-          }
-        })
-      }
+      fs.symlink(c, link, (err) => {
+        if(err) {
+          console.log("ERROR linking plugin: ", err )
+        } else {
+          console.log("Linked plugin: ", agent_name )
+        }
+      })
+      fs.symlink(dd, dlink, (err) => {
+        if(err) {
+          console.log("ERROR linking plugin: ", err )
+        }
+      })
     })
 });
