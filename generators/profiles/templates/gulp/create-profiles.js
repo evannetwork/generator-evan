8a7d30bb6262bacb61e71f13bc61a5e4a385e634
Philip Kaiser <phiipkaiser30.05@gmail.com>
2020-01-10 10:43:48 +0100

    Refactored create-profiles task

--- generators/profiles/templates/gulp/create-profiles.js
+++ generators/profiles/templates/gulp/create-profiles.js
@@ -17,56 +17,54 @@ const {
 const runtimeConfig = require('../scripts/config/deployment.js').runtimeConfig;
 const evan = require('../scripts/evan.access.js');
 
+
 let web3;
-let dfs;
 let runtimes;
 
-gulp.task('init-profiles', gulp.series(async () => {
+gulp.task('init-profiles', gulp.series([async () => {
   const provider = new Web3.providers.WebsocketProvider(runtimeConfig.web3Provider);
   web3 = new Web3(provider, null, { transactionConfirmationBlocks: 1 });
 
   await buildKeyConfig(web3, runtimeConfig);
   await checkBalances(web3, runtimeConfig);
 
-  dfs = new Ipfs({
-    dfsConfig:runtimeConfig.ipfs,
-    web3: web3,
-    accountId: runtimeConfig.accounts[0],
-    privateKey: runtimeConfig.accountMap[runtimeConfig.accounts[0]]
-  })
   runtimes = await createRuntimes(web3, runtimeConfig);
+ 
   return evan.cacheProfiles(runtimeConfig);   // so we can avoid checking on the network later
+}]))
+
+gulp.task('ensure-profiles', gulp.series(['init-profiles'], async () => {
+  await ensureProfiles(runtimes, runtimeConfig);
+  await evan.cacheProfiles(runtimeConfig);   // so we can avoid checking on the network later
 }))
 
-gulp.task('ensure-profiles', gulp.series(['init-profiles']), async () => {
-  await ensureProfiles(runtimes, runtimeConfig);
-  await evan.cacheProfiles(runtimeConfig);   // so we can avoid checking on the network later
-})
-
-gulp.task('exchange-keys', gulp.series(['init-profiles', 'ensure-profiles']), async () => {
+gulp.task('exchange-keys', gulp.series(['init-profiles', 'ensure-profiles'], async () => {
   await exchangeKeys(runtimes, runtimeConfig);
-})
+}))
 
-gulp.task('add-bookmarks', gulp.series(['init-profiles', 'ensure-profiles']), async () => {
+gulp.task('add-bookmarks', gulp.series(['init-profiles', 'ensure-profiles'], async () => {
   await addBookmarks(runtimes, runtimeConfig);
-})
+}))
 
-gulp.task('add-to-business-centers', gulp.series(['init-profiles']), async () => {
+gulp.task('add-to-business-centers', gulp.series(['init-profiles'], async () => {
   await addToBusinessCenters(runtimes, runtimeConfig);
-})
+}))
 
-gulp.task('invite-to-contracts', gulp.series(['init-profiles']), async () => {
+gulp.task('invite-to-contracts', gulp.series(['init-profiles'], async () => {
   await inviteToContracts(runtimes, runtimeConfig);
   await evan.cacheProfiles(runtimeConfig);   // so we can avoid checking on the network later
-})
+}))
 
-gulp.task('create-profiles', gulp.series(['init-profiles']), async () => {
+gulp.task('create-profiles', gulp.series(['init-profiles', async () => {
   await ensureProfiles(runtimes, runtimeConfig);
   await exchangeKeys(runtimes, runtimeConfig);
   await addBookmarks(runtimes, runtimeConfig);
   await addToBusinessCenters(runtimes, runtimeConfig);
   await inviteToContracts(runtimes, runtimeConfig);
-  evan.cacheProfiles(runtimeConfig);   // so we can avoid checking on the network later
-})
+  await evan.cacheProfiles(runtimeConfig); // so we can avoid checking on the network later
+  web3.currentProvider.disconnect(1000);
+  setTimeout(() => {console.log('Task finished successfully. Please press CTRL-C.')});
+  
+}]))
 
 gulp.task('default', gulp.series(['create-profiles']))
